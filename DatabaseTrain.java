af9752616f71838a56977f5484ddc6d95d17903f
sankyyu <syu10@stevens.edu>
2016-05-10 09:06:50 -0400

    Update DatabaseTrain.java

--- DatabaseTrain.java
+++ DatabaseTrain.java
@@ -13,11 +13,11 @@ public class DatabaseTrain {
 	public static void main(String[] arg) throws IOException{
 		try {      
             
-            ArrayList<String[]> csvList = new ArrayList<String[]>(); //用来保存数据  
+            ArrayList<String[]> csvList = new ArrayList<String[]>(); //store the data  
             String csvFilePath = "/Users/Sanky/Downloads/2.csv";  
-             CsvReader reader = new CsvReader(csvFilePath,',',Charset.forName("SJIS"));    //一般用这编码读就可以了      
+             CsvReader reader = new CsvReader(csvFilePath,',',Charset.forName("SJIS"));    //GBK or something     
                
-             //reader.readHeaders(); // 跳过表头   如果需要表头的话，不要写这句。  
+             //reader.readHeaders(); // skip the head  
                
              while(reader.readRecord()){ //逐行读入除表头的数据      
                  csvList.add(reader.getValues());  
